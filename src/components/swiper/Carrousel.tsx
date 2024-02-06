edad7d47c34db2ca454bbd044fa7e9987a29a5bb
poliferreyra <paoferreyra.fl@gmail.com>
2024-02-06 12:34:16 -0300

    Add text and links to carrousel

--- src/components/swiper/Carrousel.tsx
+++ src/components/swiper/Carrousel.tsx
@@ -5,7 +5,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "./styles.css";
 
-import { Image } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 // import required modules
 import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
 // Import Swiper React components
@@ -28,10 +29,85 @@ export const Carrousel = () => {
       className="mySwiper"
     >
       <SwiperSlide>
-        <Image src={slider1} objectFit="cover" boxSize="400px" />
+        <Box
+          w="100vw"
+          h="400px"
+          bgImage={`url(${slider1})`}
+          bgPosition="center"
+          bgRepeat="no-repeat"
+          objectFit="cover"
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Box>
+            <Heading lineHeight="1.2">
+              <Text as="mark" bg={"rgba(0, 0, 0, 0.5)"} color="white">
+                Buscamos proyectos innovadores
+              </Text>
+            </Heading>
+            <br />
+            <Heading lineHeight="0.3">
+              <Text as="mark" bg={"rgba(0, 0, 0, 0.5)"} color="white">
+                de salud física y mental
+              </Text>
+            </Heading>
+            <Button
+              as={Link}
+              to="https://mayma.lat/salud"
+              target="_blank"
+              rel="noopener noreferrer"
+              size="lg"
+              bg="#C9302C"
+              mt="24px"
+              color="white"
+              borderRadius="20px"
+            >
+              Quiero + info
+            </Button>
+          </Box>
+        </Box>
       </SwiperSlide>
+
       <SwiperSlide>
-        <Image src={slider2} objectFit="cover" boxSize="400px" />
+        <Box
+          w="100%"
+          h="400px"
+          bgImage={`url(${slider2})`}
+          bgPosition="center center"
+          bgRepeat="no-repeat"
+          objectFit="contain"
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Box>
+            <Heading lineHeight="1">
+              <Text as="mark" bgColor={"rgba(0, 0, 0, 0.5)"} color="white">
+                Hace 17 años fortalecemos emprendedores
+              </Text>
+            </Heading>
+            <br />
+            <Heading lineHeight="0.5">
+              <Text as="mark" bgColor={"rgba(0, 0, 0, 0.5)"} color="white">
+                socioambientales de Lationoamérica
+              </Text>
+            </Heading>
+            <Button
+              as={Link}
+              to="https://mayma.org.ar/hablemos?_gl=1*6so4g2*_ga*MTk0NzQ0ODMyLjE3MDcxNjEyMzU.*_ga_JZ7CXM01CB*MTcwNzIyNjk4NC4yLjEuMTcwNzIyODQxMC4wLjAuMA.."
+              target="_blank"
+              rel="noopener noreferrer"
+              size="lg"
+              bg="#0A78E4"
+              color="white"
+              mt="24px"
+              borderRadius="20px"
+            >
+              Hablemos
+            </Button>
+          </Box>
+        </Box>
       </SwiperSlide>
     </Swiper>
   );
