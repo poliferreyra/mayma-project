6ae7a8ad4f150b72ff1ddd3d1b2eccc2a3b6d157
poliferreyra <paoferreyra.fl@gmail.com>
2024-02-16 13:01:50 -0300

    Refactoring product detail layout

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,8 +1,20 @@
-import { Box, Container, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Divider,
+  Heading,
+  Stack,
+  StackDivider,
+  Tag,
+  Text,
+} from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
-import enBuenasManos from "@/assets/enBuenasManos.png";
+import { Map } from "@/components/googleMaps/Map";
 import { getProductById } from "@/services/product.service";
 
 export const ProductDetail = () => {
@@ -13,20 +25,55 @@ export const ProductDetail = () => {
     isLoading,
     isError,
   } = useQuery(["product", id], () => getProductById(id ?? ""));
+  const entity = product?.entity;
 
   if (isLoading) return <Text>Loading...</Text>;
   if (isError) return <Text>Error fetching data</Text>;
   if (!product) return <Text>No data available</Text>;
 
   return (
-    <Container>
+    <Container maxW={"1200"} mt={5}>
       {product ? (
-        <Box>
-          <Heading>{product.title}</Heading>
-          <Image src={enBuenasManos} alt={product.title} />
-          <Text dangerouslySetInnerHTML={{ __html: product.description }} />
-        </Box>
+        <Stack>
+          <Map />
+          <Box bg="red.200">
+            <Heading>{product.title}</Heading>
+          </Box>
+          <Box>
+            <Card bg="green.200">
+              <CardHeader>
+                <Heading size="md">Sobre Nosotros</Heading>
+              </CardHeader>
+
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing="4">
+                  <Box>
+                    <Text pt="2" fontSize="sm">
+                      {entity?.about_us}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Tag size="lg" variant="outline" colorScheme="blue">
+                      {product.product_type}
+                    </Tag>
+                  </Box>
+                </Stack>
+              </CardBody>
+            </Card>
+          </Box>
+          <Box bg="yellow.200">
+            <Text> {entity?.fantasy_name}</Text>
+            <Divider m="4" />
+            <Text> {entity?.address}</Text>
+            <Text> {entity?.type.name}</Text>
+          </Box>
+        </Stack>
       ) : (
+        // <Box>
+        //   <Heading>{product.title}</Heading>
+        //   <Image src={enBuenasManos} alt={product.title} />
+        //   <Text dangerouslySetInnerHTML={{ __html: product.description }} />
+        // </Box>
         <Box>No se encontró el producto</Box>
       )}
     </Container>
