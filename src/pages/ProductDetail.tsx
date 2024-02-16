8c653c42390edf223b37516e5e561067fa3fb2dd
poliferreyra <paoferreyra.fl@gmail.com>
2024-02-16 11:07:35 -0300

    Refactoring function and render product detail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,23 +1,30 @@
 import { Box, Container, Heading, Image, Text } from "@chakra-ui/react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
-import { products } from "@/services/products";
+import enBuenasManos from "@/assets/enBuenasManos.png";
+import { getProductById } from "@/services/product.service";
 
 export const ProductDetail = () => {
-  const { id } = useParams<{ id: string  }>();
+  const { id } = useParams<{ id: string }>();
 
-  // ❌ chequear el error
-   const productIdAsNumber = parseInt(id || "No exite producto");
+  const {
+    data: product,
+    isLoading,
+    isError,
+  } = useQuery(["product", id], () => getProductById(id ?? ""));
 
-  const product = products.find((p) => p.id === productIdAsNumber);
+  if (isLoading) return <Text>Loading...</Text>;
+  if (isError) return <Text>Error fetching data</Text>;
+  if (!product) return <Text>No data available</Text>;
 
   return (
     <Container>
       {product ? (
         <Box>
           <Heading>{product.title}</Heading>
-          <Image src={product.image} alt={product.title} />
-          <Text>{product.description}</Text>
+          <Image src={enBuenasManos} alt={product.title} />
+          <Text dangerouslySetInnerHTML={{ __html: product.description }} />
         </Box>
       ) : (
         <Box>No se encontró el producto</Box>
